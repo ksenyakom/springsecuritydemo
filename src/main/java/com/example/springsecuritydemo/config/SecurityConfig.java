c89b6db22e4f45f484401f46e6d828d89c013ca4
ksenys kom <ksenyakom@gmail.com>
2021-06-28 12:50:22 +0200

    2 commit pom added

--- src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JWTConfigurer jwtConfigurer;
