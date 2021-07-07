f550b008994e876910a78091ea2337cbc0d3bd98
ksenys kom <ksenyakom@gmail.com>
2021-07-07 10:18:52 +0200

    3 commit oauth added

--- src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.springsecuritydemo.config;
 
+import com.example.springsecuritydemo.model.GoogleUser;
 import com.example.springsecuritydemo.model.Permission;
 import com.example.springsecuritydemo.model.Role;
+import com.example.springsecuritydemo.model.User;
+import com.example.springsecuritydemo.repository.UserRepository;
 import com.example.springsecuritydemo.security.JWTConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,7 +21,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,6 +30,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableOAuth2Sso
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -37,18 +42,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/api/v1/auth/login").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .apply(jwtConfigurer);
+                .csrf().disable();
     }
+
+    @Bean
+    public PrincipalExtractor principalExtractor(UserRepository userRepository) {
+        return map -> {
+            String email = (String) map.get("email");
+            User googleUser = userRepository.findByEmail(email).orElseGet(() -> {
+                    User user = new User();
+                    user.setEmail(email);
+                    user.setName((String)map.get("given_name"));
+                    user.setSurname((String)map.get("family_name"));
+                    user.setRole(Role.USER);
+                    user.setActive(true);
+                    return userRepository.save(user);
+            });
+
+            return googleUser;
+        };
+    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/api/v1/auth/login").permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .apply(jwtConfigurer);
+//    }
 
     @Bean
     protected PasswordEncoder passwordEncoder() {
--- src/main/java/com/example/springsecuritydemo/model/User.java
+++ src/main/java/com/example/springsecuritydemo/model/User.java
@@ -24,4 +24,6 @@ public class User {
     private Role role;
     private boolean isActive;
 
+    public User() {
+    }
 }
